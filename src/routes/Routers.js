8d8d6b10dfe492cf14daa2e5574fac597cc2438e
ion <delion.dev@gmail.com>
2024-06-29 00:23:47 +0900

    [Feat #3]: Routers.js에 LandingPage 경로 추가

--- src/routes/Routers.js
+++ src/routes/Routers.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// import LandingPage from "../pages/landing/LandingPage.js";
+import LandingPage from "../pages/landing/LandingPage.js";
 
 const Routers = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/" element={<LandingPage />} /> */}
+        <Route path="/" element={<LandingPage />} />
       </Routes>
     </BrowserRouter>
   );
