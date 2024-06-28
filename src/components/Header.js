946fd1d72a914c6fb3adb647251283dac0e0143b
ion <delion.dev@gmail.com>
2024-06-29 00:19:56 +0900

    [Feat #3]: Header js 및 css 작성

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import "./Header.css";
+
+const Header = () => {
+  const navigate = useNavigate();
+  
+  const logoClick = () => {navigate('/');};
+
+  return (
+    <div className="header">
+      <div className="background"/>
+      <div className="foreground header-group">
+        <div className="header-logo" onClick={logoClick}>
+          <img src="TalkPossible_logo.png" alt="img" />
+        </div>
+        <div className="group-menu">
+          <div className="text-color-change-sign">회원가입</div>
+          <div className="text-color-change-sign">로그인</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
