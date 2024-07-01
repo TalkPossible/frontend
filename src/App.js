d80e18608b0136c8201047f9723ea2b7c4ba087e
jeonggggggggggg <120429419+jeonggggggggggg@users.noreply.github.com>
2024-07-02 02:22:56 +0900

    사용자 실시간으로 포즈 인식하기, mediapipe이용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
+import PoseDetection from './pages/motion/motiondetection';
 
 import Routers from './routes/Routers.js';
 
@@ -8,6 +9,7 @@ function App() {
     <ChakraProvider>
       <div className="App">
         <Routers/>
+        <PoseDetection></PoseDetection>
       </div>
     </ChakraProvider>
   );
