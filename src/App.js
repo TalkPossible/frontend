a50cb4838ce6c510961e04b015c57a82876b7f3d
jeonggggggggggg <120429419+jeonggggggggggg@users.noreply.github.com>
2024-07-02 18:58:34 +0900

    motiondetection page 경로 수정

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
-import PoseDetection from './pages/motion/motiondetection';
-
 import Routers from './routes/Routers.js';
 
 function App() {
   return (
     <ChakraProvider>
       <div className="App">
-        <Routers/>
-        <PoseDetection></PoseDetection>
+        <Routers />
       </div>
     </ChakraProvider>
   );
