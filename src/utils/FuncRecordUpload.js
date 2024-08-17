e0af6d353fc6d69f8ca64bd6e15b426aba502220
ion <delion.dev@gmail.com>
2024-08-17 22:14:38 +0900

    [Feat #26]: 음성녹음 및 업로드 함수 작성

---
+++ src/utils/FuncRecordUpload.js
@@ -0,0 +1,72 @@
+import { BlobServiceClient } from '@azure/storage-blob';
+
+const storageAccAzure = process.env.REACT_APP_AZURE_STORAGE_ACC;
+const storageContainerNameAzure = process.env.REACT_APP_AZURE_STORAGE_CONTAINER_NAME;
+const storageSasTokenAzure = process.env.REACT_APP_AZURE_STORAGE_SAS_TOKEN;
+
+export const onRecAudio = (setStream, setMedia, setOnRec, setSource, setAnalyser, setAudioUrl) => {
+  const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+  const analyser = audioCtx.createScriptProcessor(0, 1, 1);
+  setAnalyser(analyser);
+
+  function makeSound(stream) {
+    const source = audioCtx.createMediaStreamSource(stream);
+    setSource(source);
+    source.connect(analyser);
+    analyser.connect(audioCtx.destination);
+  }
+
+  navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+    const mediaRecorder = new MediaRecorder(stream);
+
+    mediaRecorder.addEventListener('dataavailable', (e) => {
+      setAudioUrl(e.data);
+      // console.log("FuncRecordUpload에서 녹음시작에서 오디오 url 확인 : ", e.data);
+    });
+
+    mediaRecorder.start();
+    setStream(stream);
+    setMedia(mediaRecorder);
+    makeSound(stream);
+
+    analyser.onaudioprocess = function (e) {
+      setOnRec(false);
+    };
+  }).catch((error) => {
+    alert('마이크 사용 권한을 허용해야 녹음을 진행할 수 있습니다.');
+  });
+};
+
+export const offRecAudio = (stream, media, analyser, source, setOnRec) => {
+  stream.getAudioTracks().forEach(function (track) {
+    track.stop();
+  });
+
+  media.stop();
+  analyser.disconnect();
+  source.disconnect();
+
+  setOnRec(true);
+};
+
+export const onSubmitAudioFile = async (audioUrl) => {
+  if (audioUrl) {
+    const time = new Date().getTime();
+    const fileName = "userID" + "_" + "simulID" + "_" + time;
+    const sound = new File([audioUrl], `${fileName}.wav`, { lastModified: new Date().getTime(), type: "audio/wav" });
+
+    const account = storageAccAzure;
+    const storageSasToken = storageSasTokenAzure;
+    const containerName = storageContainerNameAzure;
+
+    const blobServiceClient = new BlobServiceClient(`https://${account}.blob.core.windows.net/?${storageSasToken}`);
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+    const blockBlobClient = containerClient.getBlockBlobClient(sound.name);
+
+    await blockBlobClient.uploadBrowserData(sound);
+
+    return fileName;
+
+    // console.log(`File uploaded to Azure Blob Storage: ${blockBlobClient.url}`);
+  }
+};
