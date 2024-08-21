86379134e2a05f19e0cdc23921bb5bae9ba837e6
ion <delion.dev@gmail.com>
2024-08-21 21:07:00 +0900

    [Feat #26]: simulationId 얻을 수 있도록 api 연결

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,8 +2,34 @@ import React from 'react';
 
 import '../assets/css/Modal.css';
 
+import {API_BASE_URL} from '../api/apiConfig.js';
+
 const Modal = (props) => {
   const { open, close } = props;
+
+  const handleStartBtn = async () => {
+    try {
+      const response = await fetch(`${API_BASE_URL}/api/v1/simulations`, {
+        method: 'POST',
+        headers: {
+          'Authorization': `Bearer ${localStorage.getItem('accessToken')}`,
+          'patientId': localStorage.getItem('patientId'), 
+          'situationId': localStorage.getItem('situationId'), 
+        }
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to create simulation');
+      }
+
+      const data = await response.json();
+      localStorage.setItem('simulationId', data.simulationId);
+      console.log('Simulation created with ID:', data.simulationId);
+    } catch (error) {
+      console.error("Error creating simulation:", error);
+      return; // 시뮬레이션 생성 실패 시 이후 로직 실행하지 않음
+    }
+  }
 
   return (
     <div className={open? 'openModal modal' : 'modal'}>
@@ -13,7 +39,7 @@ const Modal = (props) => {
             &times;
           </button>
           <main className='content-box'>{props.children}</main>
-          <button className='startBtn' >
+          <button className='startBtn' onClick={handleStartBtn}>
             시작하기
           </button>
         </section>
