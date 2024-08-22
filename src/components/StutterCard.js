172aae4b75dc3a6c557350db8abaa37c8bae0c9e
ion <delion.dev@gmail.com>
2024-08-22 22:51:35 +0900

    [Refactor #23]: 말더듬 카드 css 수정

--- src/components/StutterCard.js
+++ src/components/StutterCard.js
@@ -1,27 +1,27 @@
 import React, { useRef } from 'react';
 
+import '../assets/css/StutterCard.css';
+
 const StutterCard = ({stuttered}) => {
   const audioRef = useRef(null);
 
   return (
-    <div style={{height: '100%', display: 'flex', flexDirection: 'column', justifyContent:'space-between'}}>
+    <div className="card-body">
       <div>
-        <img src={stuttered.imageUrl} alt="스펙트로그램 이미지" style={{ width: '100%', borderRadius: '8px' }} />
-        <p style={{textAlign: 'center'}}>(사진에 대한 설명)</p>
+        <img className='card-img' src={stuttered.imageUrl} alt="스펙트로그램 이미지" />
+        <p className='card-img-desc' >(사진에 대한 설명)</p>
       </div>
 
       <div>
-        <div>다시 듣기</div>
-        <div>
-          <audio ref={audioRef} src="your_audio_file_url_here" controls style={{ width: '100%' }}>
-            브라우저가 해당 오디오 형식을 지원하지 않습니다. 
-          </audio>
-        </div>
+        <div className='card-sub-title'>다시 듣기</div>
+        <audio ref={audioRef} src="your_audio_file_url_here" controls >
+          브라우저가 해당 오디오 형식을 지원하지 않습니다. 
+        </audio>
       </div>
 
       <div>
-        <div>발화 문장</div>
-        <div>{stuttered.text}</div>
+        <div className='card-sub-title'>발화 문장</div>
+        <div className='card-script'>{stuttered.text}</div>
       </div>
 
     </div>
