371ec3ed532e61a9948f1f91d97785d9d0d5f353
ion <delion.dev@gmail.com>
2024-08-22 22:51:12 +0900

    [Refactor #23]: 대화 내역 챗 버블 css 수정

--- src/components/ChatBubble.js
+++ src/components/ChatBubble.js
@@ -1,13 +1,13 @@
+import "../assets/css/ChatBubble.css";
+
 export const LeftBubble = (props) => (
- <div style={{backgroundColor:'#fff', maxWidth:'300px', borderRadius:'10px 10px 10px 0px',
-  color: '#000', padding: '6px'}}>
+ <div className="bubble bubble-1st" >
   {props.children}
  </div>
 )
 
 export const RightBubble = (props) => (
-  <div style={{backgroundColor:'#000', maxWidth:'300px', borderRadius:'10px 0px 10px 10px',
-    color: '#fff', padding: '6px'}}>
+  <div className="bubble bubble-2nd" >
   {props.children}
   </div>
  )
