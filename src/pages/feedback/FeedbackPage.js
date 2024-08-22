802de573646b1d5a9a0dae3d6d1c7aa820cde0d3
ion <delion.dev@gmail.com>
2024-08-22 23:55:40 +0900

    [Feat #23]: menu 클릭시 애니메이션 적용

--- src/pages/feedback/FeedbackPage.js
+++ src/pages/feedback/FeedbackPage.js
@@ -19,29 +19,83 @@ const FeedbackPage = () => {
   const menuConversation = useRef(null);
   const menuVoice = useRef(null);
   const menuMotion = useRef(null);
+
+  const [isAnimating, setIsAnimating] = useState(false);
+
   const toMenuListClicked = (e) => {
+    if (isAnimating) return;  // 애니메이션 중에는 클릭 불가
+
     const parentEl = e.currentTarget.closest('.fd-menu');
     if (parentEl) {
-      parentEl.style.display = 'none'; 
-    };
-    menuList.current.style.display = 'flex';
+      parentEl.classList.add('fade-out');
+
+      setTimeout(() => {
+        parentEl.style.display = 'none'; 
+        menuList.current.style.display = 'flex';
+        menuList.current.classList.add('fade-in');
+
+        setIsAnimating(false);
+        setTimeout(() => {
+          parentEl.classList.remove('fade-out');
+          menuList.current.classList.remove('fade-in');
+        }, 500);  
+      }, 500);  // 애니메이션 지속 시간과 일치하도록 500ms 설정
+    }
   };
   const conversationClicked = () => {
-    menuList.current.style.display = 'none';
-    menuConversation.current.style.display = 'flex';
-    menuConversation.current.style.flexDirection = 'column';
+    setIsAnimating(true);
+    menuList.current.classList.add('fade-out');
+    
+    setTimeout(() => { // 애니메이션이 끝난 후 실행
+      menuList.current.style.display = 'none';
+      menuConversation.current.style.display = 'flex';
+      menuConversation.current.style.flexDirection = 'column';
+  
+      menuConversation.current.classList.add('fade-in-up'); 
+
+      setIsAnimating(false);
+      setTimeout(() => {
+        menuList.current.classList.remove('fade-out');
+        menuConversation.current.classList.remove('fade-in-up');
+      }, 500);
+    }, 500); 
   };
   const voiceClicked = () => {
-    menuList.current.style.display = 'none';
-    menuVoice.current.style.display = 'flex';
-    menuVoice.current.style.flexDirection = 'column';
+    setIsAnimating(true);
+    menuList.current.classList.add('fade-out');
+
+    setTimeout(() => {
+      menuList.current.style.display = 'none';
+      menuVoice.current.style.display = 'flex';
+      menuVoice.current.style.flexDirection = 'column';
+
+      menuVoice.current.classList.add('fade-in-up');
+
+      setIsAnimating(false);
+      setTimeout(() => {
+        menuList.current.classList.remove('fade-out');
+        menuVoice.current.classList.remove('fade-in-up');
+      }, 500);
+    }, 500);
   };
   const motionClicked = () => {
-    menuList.current.style.display = 'none';
-    menuMotion.current.style.display = 'flex';
-    menuMotion.current.style.flexDirection = 'column';
-  };
+    setIsAnimating(true);
+    menuList.current.classList.add('fade-out');
 
+    setTimeout(() => {
+      menuList.current.style.display = 'none';
+      menuMotion.current.style.display = 'flex';
+      menuMotion.current.style.flexDirection = 'column';
+
+      menuMotion.current.classList.add('fade-in-up');
+
+      setIsAnimating(false);
+      setTimeout(() => {
+        menuList.current.classList.remove('fade-out');
+        menuMotion.current.classList.remove('fade-in-up');
+      }, 500);
+    }, 500);
+  };
 
   const [currentIndex, setCurrentIndex] = useState(0);
   const handleNext = () => {
@@ -71,85 +125,85 @@ const FeedbackPage = () => {
             </div>
           </div>
 
-          <div className="fd-video">
-            video
-          </div>
+          <div className="fd-video"/>
         </div>
 
-        <div className="fd-menu menu-list" ref={menuList}>
-          <div className="sub-menu">
-            <p className="header-title">menu</p>
-            <div className="menu-box">
-              <div onClick={conversationClicked}><p>대화 내용</p></div>
-              <div onClick={voiceClicked}><p>음성 피드백</p></div>
-              <div onClick={motionClicked}><p>행동 피드백</p></div>
+        <div>
+          <div className="fd-menu menu-list" ref={menuList}>
+            <div className="sub-menu">
+              <p className="header-title">menu</p>
+              <div className="menu-box">
+                <div onClick={conversationClicked}><p>대화 내용</p></div>
+                <div onClick={voiceClicked}><p>음성 피드백</p></div>
+                <div onClick={motionClicked}><p>행동 피드백</p></div>
+              </div>
+            </div>
+
+            <div className="sub-info">
+              <p className="header-title">info</p>
+              <div className="info-box">
+                <div className="sub-container">
+                  <p className="sub-title">전체 시간</p>
+                  <p className="sub-value">3분 42초</p>
+                </div>
+                <div className="sub-container">
+                  <p className="sub-title">분당 어절수</p>
+                  <p className="sub-value">210어절/1분</p>
+                </div>
+                <div className="sub-container">
+                  <p className="sub-title">모션 감지 횟수</p>
+                  <p className="sub-value">{motionDataList[1]["motionCount"]}번</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="menu-footer">
+              <button className="fd-confirmBtn" onClick={checkBtn}>확인완료</button>
             </div>
           </div>
 
-          <div className="sub-info">
-            <p className="header-title">info</p>
-            <div className="info-box">
-              <div className="sub-container">
-                <p className="sub-title">전체 시간</p>
-                <p className="sub-value">3분 42초</p>
-              </div>
-              <div className="sub-container">
-                <p className="sub-title">분당 어절수</p>
-                <p className="sub-value">210어절/1분</p>
-              </div>
-              <div className="sub-container">
-                <p className="sub-title">모션 감지 횟수</p>
-                <p className="sub-value">{motionDataList[1]["motionCount"]}번</p>
-              </div>
+          <div className="fd-menu menu-conversation" ref={menuConversation}>
+            <div className="menu-detail-header">
+              <div className="header-title">대화 내용</div> <button onClick={toMenuListClicked}>&times;</button>
             </div>
-          </div>
 
-          <div className="menu-footer">
-            <button className="fd-confirmBtn" onClick={checkBtn}>확인완료</button>
-          </div>
-        </div>
-
-        <div className="fd-menu menu-conversation" ref={menuConversation}>
-          <div className="menu-detail-header">
-            <div className="header-title">대화 내용</div> <button onClick={toMenuListClicked}>&times;</button>
-          </div>
-
-          <div className="part-rest part-scroll">
-          {conversationList.map((txt, index) => (
-            <div key={index} style={{display: 'flex', justifyContent: txt.speaker === 'chatgpt' ? 'flex-start' : 'flex-end', 
-            width: '100%', marginBottom: '10px'}}>
-              {txt.speaker === 'chatgpt' ? 
-                <LeftBubble key={index}>{txt.content}</LeftBubble> :  
-                <RightBubble key={index}>{txt.content}</RightBubble>}
-            </div>
-          ))}
-          </div>
-        </div>
-
-        <div className="fd-menu menu-voice" ref={menuVoice}>
-          <div className="menu-detail-header">
-            <div className="header-title">말더듬 분석</div> <button onClick={toMenuListClicked}>&times;</button>
-          </div>
-
-          <div className="part-rest">
-            <StutterCard stuttered={currentResult}></StutterCard>
-          </div>
-
-          <div className="btn-pn" >
-            <button onClick={handlePrev} style={{ fontSize: '16px' }}>Prev</button>
-            <button onClick={handleNext} style={{ fontSize: '16px' }}>Next</button>
-          </div>
-        </div>
-
-        <div className="fd-menu menu-motion" ref={menuMotion}>
-          <div className="menu-detail-header">
-            <div className="header-title">동작 분석</div> <button onClick={toMenuListClicked}>&times;</button>
-          </div>
-          
-          <div className="part-rest part-scroll ">
-            {motionDataList[0]["motionList"].map((motion, index) => (
-              <p key={index} className="motion"><span>{motion.timestamp}</span> <span>{motion.motionName}</span></p>
+            <div className="part-rest part-scroll">
+            {conversationList.map((txt, index) => (
+              <div key={index} style={{display: 'flex', justifyContent: txt.speaker === 'chatgpt' ? 'flex-start' : 'flex-end', 
+              width: '100%', marginBottom: '10px'}}>
+                {txt.speaker === 'chatgpt' ? 
+                  <LeftBubble key={index}>{txt.content}</LeftBubble> :  
+                  <RightBubble key={index}>{txt.content}</RightBubble>}
+              </div>
             ))}
+            </div>
+          </div>
+
+          <div className="fd-menu menu-voice" ref={menuVoice}>
+            <div className="menu-detail-header">
+              <div className="header-title">말더듬 분석</div> <button onClick={toMenuListClicked}>&times;</button>
+            </div>
+
+            <div className="part-rest">
+              <StutterCard stuttered={currentResult}></StutterCard>
+            </div>
+
+            <div className="btn-pn" >
+              <button onClick={handlePrev} style={{ fontSize: '16px' }}>Prev</button>
+              <button onClick={handleNext} style={{ fontSize: '16px' }}>Next</button>
+            </div>
+          </div>
+
+          <div className="fd-menu menu-motion" ref={menuMotion}>
+            <div className="menu-detail-header">
+              <div className="header-title">동작 분석</div> <button onClick={toMenuListClicked}>&times;</button>
+            </div>
+            
+            <div className="part-rest part-scroll ">
+              {motionDataList[0]["motionList"].map((motion, index) => (
+                <p key={index} className="motion"><span>{motion.timestamp}</span> <span>{motion.motionName}</span></p>
+              ))}
+            </div>
           </div>
         </div>
 
