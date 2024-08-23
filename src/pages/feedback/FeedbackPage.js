c670f9265b90cd88947907cd730c924cf1e5b137
ion <delion.dev@gmail.com>
2024-08-23 09:20:23 +0900

    [Feat #51]: 시뮬레이션 영상 url 적용

--- src/pages/feedback/FeedbackPage.js
+++ src/pages/feedback/FeedbackPage.js
@@ -153,7 +153,15 @@ const FeedbackPage = () => {
             </div>
           </div>
 
-          <div className="fd-video"/>
+            <div className="fd-video">
+              <iframe title="cam video player"
+                width="100%" 
+                height="100%" 
+                src={infoUrl.videoUrl}
+                frameBorder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                allowFullScreen>
+              </iframe>
         </div>
 
         <div>
